0d4992db2ffbce37a1541ae9da7b113f0007873c
yogesh121415 <ayogesh1214@gmail.com>
2022-12-22 19:48:08 +0530

    hashrouter to index

--- src/App.js
+++ src/App.js
@@ -26,13 +26,13 @@ const App = ()=> {
      <Navbar/>
      <Routes>
      <Route exact path="/"  element={<News bar={setProgress} key="general" pagesize={13} country="in" category="general"/> }></Route>
-     <Route  path="/sports"  element={<News bar={setProgress} key="sports" pagesize={13} country="in" category="sports"/> }></Route>
-     <Route  path="/business"  element={<News bar={setProgress} key="business" pagesize={13} country="in" category="business"/> }></Route>
-     <Route  path="/entertainment"  element={<News bar={setProgress} key="entertainment"pagesize={13} country="in" category="entertainment"/> }></Route>
-     <Route  path="/science" element={<News bar={setProgress} key="science"  pagesize={13} country="in" category="science"/> }></Route>
-     <Route  path="/health"  element={<News bar={setProgress} key="health" pagesize={13} country="in" category="health"/> }></Route>
-     <Route  path="/general"  element={<News bar={setProgress} key="general"pagesize={13} country="in" category="general"/> }></Route>
-     <Route  path="/technology" element={<News bar={setProgress} key="technology"  pagesize={13} country="in" category="technology"/> }></Route>
+     <Route   exact path ="/sports"  element={<News bar={setProgress} key="sports" pagesize={13} country="in" category="sports"/> }></Route>
+     <Route  exact path="/business"  element={<News bar={setProgress} key="business" pagesize={13} country="in" category="business"/> }></Route>
+     <Route  exact path="/entertainment"  element={<News bar={setProgress} key="entertainment"pagesize={13} country="in" category="entertainment"/> }></Route>
+     <Route  exact path="/science" element={<News bar={setProgress} key="science"  pagesize={13} country="in" category="science"/> }></Route>
+     <Route  exact path="/health"  element={<News bar={setProgress} key="health" pagesize={13} country="in" category="health"/> }></Route>
+     <Route  exact path="/general"  element={<News bar={setProgress} key="general"pagesize={13} country="in" category="general"/> }></Route>
+     <Route  exact path="/technology" element={<News bar={setProgress} key="technology"  pagesize={13} country="in" category="technology"/> }></Route>
      </Routes>
      </Router>
      
